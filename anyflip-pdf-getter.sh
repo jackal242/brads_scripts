cd33017fe160deb6319c328ad64cd52563d0e45d
jackal242 <brad.allison@gmail.com>
2023-10-25 02:52:42 -0400

    adding

---
+++ anyflip-pdf-getter.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# 
+# Script to download pdf's from anyflip.com
+#  -- downloads all jpg images
+#  -- compiles them (the jpg images) together in order with imagemagick into a pdf file
+#  -- then OCR's the pdf and creates a final pdf with OCR text overload (so you can search the pdf)
+#
+# Requires:  ImageMagik for the convert command
+#  Use: brew install imagemagick
+#
+# Requires: ocemypdf
+#  Use: brew install ocrmypdf
+#
+#
+
+#echo     $0 https://online.anyflip.com/jbwjh/mals/files/mobile
+URL=$1
+
+
+function usage() {
+	echo   USAGE:
+	echo     $0 https://online.anyflip.com/jbwjh/mals/
+	exit;
+}
+
+if [[ $URL == "" ]] ; then
+  usage ;
+fi
+
+require_string='online.anyflip.com'
+if [[ $URL == *"$require_string"* ]]; then
+  true
+else
+  echo "Please change the hostname to include --- > $require_string"
+  usage ;
+fi
+
+
+for i in {1..10000}; do 
+	#################
+	# Handle leading 0's for order
+	#################
+	if [[ "$i" -lt "10" ]]; then
+   		j=00$i
+	elif [[ "$i" -lt "100" ]]; then
+   		j=0$i
+	else
+   		j=$i
+	fi
+
+	echo curl -s ${URL}files/mobile/$i.jpg -o $j.jpg; 
+	curl -s ${URL}files/mobile/$i.jpg -o $j.jpg; 
+	file $j.jpg |grep JPEG > /dev/null
+	if [[ "$?" -gt "0" ]]; then
+		echo breaking at $j.jpg
+		rm $j.jpg
+		break
+        fi
+done
+
+convert *.jpg -auto-orient combined.pdf
+ocrmypdf combined.pdf FINAL-ocr.pdf
+
